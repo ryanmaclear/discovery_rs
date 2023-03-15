d82188925ac5cf8725e80f72e249982cff072cb1
Ryan <ryan.maclear@gmail.com>
2023-03-15 20:25:06 +0200

    Magnetic field magnitude

--- microbit/src/09-led-compass/src/main.rs
+++ microbit/src/09-led-compass/src/main.rs
@@ -14,8 +14,7 @@ mod led;
 use crate::led::Direction;
 use crate::led::direction_to_led;
 
-use core::f32::consts::PI;
-use libm::atan2f;
+use libm::sqrtf;
 
 use microbit::{display::blocking::Display, hal::Timer};
 
@@ -47,29 +46,12 @@ fn main() -> ! {
         let mut data = sensor.mag_data().unwrap();
         data = calibrated_measurement(data, &calibration);
 
-        let theta = atan2f(data.y as f32, data.x as f32);
+        let x = data.x as f32;
+        let y = data.y as f32;
+        let z = data.z as f32;
 
-        let dir = 
-            if theta < -7. * PI / 8. {
-                Direction::West
-            } else if theta < -5. * PI / 8. {
-                Direction::SouthWest
-            } else if theta < -3. * PI / 8. {
-                Direction::South
-            } else if theta < -PI / 8. {
-                Direction::SouthEast
-            } else if theta < PI / 8. {
-                Direction::East
-            } else if theta < -3. * PI / 8. {
-                Direction::NorthEast
-            } else if theta < -5. * PI / 8. {
-                Direction::North
-            } else if theta < -7. * PI / 8. {
-                Direction::NorthWest
-            } else {
-                Direction::West
-            };
+        let magnitude = sqrtf(x*x + y*y + z*z);
 
-        display.show(&mut timer, direction_to_led(dir), 100);
+        rprintln!("{} nT, {} mG", magnitude, magnitude/100.0);
     }
 }
